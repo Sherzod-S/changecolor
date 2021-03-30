e7acc63507c047e87cb11a47ec50015b16d21646
SherzodSherzodSherzod <Classik55@mail.ru>
2021-03-30 22:18:24 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let btnPurple = document.querySelector("#btn-purple");
+let btnOrange = document.querySelector("#btn-orange");
+let btnPink = document.querySelector("#btn-pink");
+let btnGreen = document.querySelector("#btn-green");
+let btnBlue = document.querySelector("#btn-blue"); 
+ 
+ 
+btnPurple.addEventListener('click', () => { 
+    document.body.style.background = 'purple'
+})
+btnOrange.addEventListener('click', () => { 
+    document.body.style.background = 'orange'
+})
+btnPink.addEventListener('click', () => { 
+    document.body.style.background = 'pink'
+})
+btnGreen.addEventListener('click', () => { 
+    document.body.style.background = 'green'
+})
+btnBlue.addEventListener('click', () => { 
+    document.body.style.background = 'blue'
+})
